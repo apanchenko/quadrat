5b03d1c230a7472db1f36eb6ffc89498ca5d8f49
Anton <apanchenko@gmail.com>
2019-05-12 22:42:05 +0300

    separate core to luacor using submodule

--- src/core/bld.lua
+++
@@ -1,7 +0,0 @@
--- build configurations
-return 
-{
-  debug   = { id = 1, name = 'debug'},
-  develop = { id = 2, name = 'develop'},
-  release = { id = 3, name = 'release' },
-}
--- src/core/package.lua
+++
@@ -1,5 +0,0 @@
-local pkg = require 'src.core.pkg'
-
-return pkg
-  :new('src.core')
-  :load('arr', 'ass', 'cnt', 'env', 'evt', 'lay', 'log', 'map', 'obj', 'typ', 'vec', 'wrp')
