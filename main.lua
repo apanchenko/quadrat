f73a8f573148b739c14aa1382dc9ebe8ea50acb0
Anton <apanchenko@gmail.com>
2019-06-29 15:21:42 +0300

    clean log configurations

--- main.lua
+++ main.lua
@@ -3,7 +3,7 @@ local log = require 'src.lua-cor.log'
 local env = require 'src.lua-cor.env'
 
 -- select 'debug', 'dev', 'release'
-log.set_configuration('dev')
+log.set_debug()
 --log.get('view').disable()
 env.cfg = require 'src.cfg'
 env.log = log
