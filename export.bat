c1e80396a124e6c4838eada13b8314e933b6f57b
Anton Panchenko <apanchenko@gmail.com>
2019-09-26 00:10:42 +0300

    export resources

--- export.bat
+++ export.bat
@@ -1,9 +1,9 @@
 cd ./res
 
-set list=sphere cell parasite
+set list=sphere cell parasite scavenger
 
 for %%a in (%list%) do (
-  inkscape --export-id=%%a --export-id-only -e %%a.png piece_qr.svg
+  inkscape --export-id=%%a --export-id-only -a -128:-128:128:128 -e %%a.png piece_qr.svg
 )
 
 cd ..
